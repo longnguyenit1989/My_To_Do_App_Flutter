d600d5d71c683c245edab2053456a6e984811ec1
HaiLT <hai.le.thanh@ofatechjsc.com>
2023-03-29 14:42:30 +0700

    Add firebase firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'ui/home.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // final db = FirebaseFirestore.instance;
+  // final quote = <String, dynamic>{
+  //   "content": "Captain Teemo on duty",
+  //   "author": "Teemo"
+  // };
+
+  // db
+  //     .collection("quotes")
+  //     .add(quote)
+  //     .then((value) => print('DocumentSnapshot added with ID: ${value.id}'));
+
   runApp(const MyApp());
 }
 
